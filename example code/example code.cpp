0178cd5425ce8bc3e57ce5331f5993982b3e28f6
Bruno Van de Velde <vdv_b@hotmail.com>
2012-07-17 15:13:58 +0200

    Updated the example code to the new functions

--- example code/example code.cpp
+++ example code/example code.cpp
@@ -9,24 +9,25 @@ void init(tgui::Window& window)
     // As you can see you will receive a pointer to the picture.
     // This pointer may go out of scope, the real object is stored inside the window.
     // The load function for a picture points to a file, unlike the load function from other objects.
-    tgui::Picture* picture = window.addPicture();
+    tgui::Picture* picture = window.add<tgui::Picture>();
     picture->load("ThinkLinux.jpg");
+    picture->setSize(800, 600);
 
     // Create the button.
     // This time the load function contains a pathname. The folder were it points to must
     // constain an info.txt file and the necessary images. The info.txt file is very complex
-    // so it won't be discussed here (See the "read me" file inside the objects folder).
-    tgui::Button* button = window.addButton();
+    // so it won't be discussed here (See the 'read me' file inside the objects folder).
+    tgui::Button* button = window.add<tgui::Button>();
     button->load("TGUI/objects/Button/Black");
     button->setText("Button 1");
-    button->setScale(0.6f, 0.6f);
+    button->setSize(160, 40);
     button->setPosition(20, 4);
 
     // Create the checkbox.
     // This time we pass a parameter to the add function.
     // Outside the init function, our pointer will be gone, but of course you might
     // still need to access the object. You will need the string to do that.
-    tgui::Checkbox* checkbox = window.addCheckbox("CHECK");
+    tgui::Checkbox* checkbox = window.add<tgui::Checkbox>("CHECK");
     checkbox->load("TGUI/objects/Checkbox/Black");
     checkbox->setText("Checkbox");
     checkbox->setPosition(20, 50);
@@ -34,14 +35,16 @@ void init(tgui::Window& window)
     // Create the first radio button.
     // Only one radio button can be checked at a time, unless they have different parents.
     // In this example all radio buttons are added to the same parent, the window.
-    // You can create multiple panels to make multiple radio button groups.
-    tgui::RadioButton* radioButton1 = window.addRadioButton();
+    // You can use panels to make multiple radio button groups.
+    tgui::RadioButton* radioButton1 = window.add<tgui::RadioButton>();
     radioButton1->load("TGUI/objects/RadioButton/Black");
     radioButton1->setText("Radio button 1");
     radioButton1->setPosition(20, 100);
 
     // Create the second radio button.
-    tgui::RadioButton* radioButton2 = window.addRadioButton();
+    // It is loaded with the same path as the previous radio button.
+    // The images won't be loaded again, they will simply be reused.
+    tgui::RadioButton* radioButton2 = window.add<tgui::RadioButton>();
     radioButton2->load("TGUI/objects/RadioButton/Black");
     radioButton2->setText("Radio button 2");
     radioButton2->setPosition(20, 150);
@@ -50,25 +53,25 @@ void init(tgui::Window& window)
     // The borders can be changed with the setBorders function.
     // You can call setTextSize to change the size of the text. If you do not do this and leave it 0,
     // like in this example, the text will be auto scaled and fit perfectly within those borders.
-    tgui::EditBox* editBox = window.addEditBox();
+    tgui::EditBox* editBox = window.add<tgui::EditBox>();
     editBox->load("TGUI/objects/EditBox/Black");
     editBox->setBorders(6, 4, 6, 4);
     editBox->setText("This text no longer fits inside the EditBox.");
     editBox->setPosition(20, 300);
-    editBox->setSize(360, 40); // Does the same as SetScale(3, 1), but can be called after setText.
+    editBox->setSize(360, 40);
 
     // Create the slider.
     // We want the slider to lie horizontal, so we set vertical scroll to false.
-    tgui::Slider* slider = window.addSlider();
+    tgui::Slider* slider = window.add<tgui::Slider>();
     slider->load("TGUI/objects/Slider/Black");
     slider->setVerticalScroll(false);
     slider->setPosition(30, 360);
 
     // Create the loading bar.
-    // The maximum is changed, so the loading bar goes from 0 to 500.
+    // The maximum is changed, so the loading bar goes from 0 to 500 (instead of 0 to 100).
     // Note that we change callbackID here. This ID is used to receive callbacks.
     // The ID is 0 by default which means that no callback is sent.
-    tgui::LoadingBar* loadingBar = window.addLoadingBar("LOAD");
+    tgui::LoadingBar* loadingBar = window.add<tgui::LoadingBar>("LOAD");
     loadingBar->load("TGUI/objects/LoadingBar/Black");
     loadingBar->setPosition(20, 400);
     loadingBar->setMaximum(500);
@@ -78,7 +81,7 @@ void init(tgui::Window& window)
     // You can call the setLowValue function to change when the scrollbar should be visible.
     // The number is 1 by default and the scrollbar is only visible when the maximum is higher.
     // Here we set the maximum to 5.
-    tgui::Scrollbar* scroll = window.addScrollbar();
+    tgui::Scrollbar* scroll = window.add<tgui::Scrollbar>();
     scroll->load("TGUI/objects/Scrollbar/Black");
     scroll->setLowValue(4);
     scroll->setMaximum(5);
@@ -96,7 +99,7 @@ void init(tgui::Window& window)
     // the scrollbar. This path works like that from all the other objects.
     // The last parameter is the height of one item (if not provided this is a tenth of the height).
     // Again you can set the borders with the setBorders function.
-    tgui::Listbox* listbox = window.addListbox();
+    tgui::Listbox* listbox = window.add<tgui::Listbox>();
     listbox->load(260, 200, "TGUI/objects/Scrollbar/Black", 30);
     listbox->setBorders(4, 4, 4, 4);
     listbox->setPosition(300, 20);
@@ -113,13 +116,13 @@ void init(tgui::Window& window)
 
     // Create the combo box.
     // The first parameter of the load function is normal. It is the path were the arrow image can
-    // be found. The second parameter changes the width of the combo box.
+    // be found. The second and third parameters change the width and height of the combo box.
     // The next parameter would choose the amount of items in the list. If there is no scrollbar then
     // this will also be the maximum items to select from (this parameter is 10 by default).
     // If there is a scrollbar then it will become visible when you have more items than this number.
     // The last parameter would be the path to the scrollbar.
-    tgui::ComboBox* comboBox = window.addComboBox();
-    comboBox->load("TGUI/objects/ComboBox/Black", 200);
+    tgui::ComboBox* comboBox = window.add<tgui::ComboBox>();
+    comboBox->load("TGUI/objects/ComboBox/Black", 200, 40);
     comboBox->setBorders(4, 4, 4, 4);
     comboBox->setPosition(500, 330);
     comboBox->addItem("Item 1 is too long");
@@ -138,6 +141,12 @@ void init(tgui::Window& window)
     // We can do exactly the same with our combo box (same function, same parameters).
     comboBox->changeColors(sf::Color( 50,  50,  50), sf::Color(200, 200, 200),
                            sf::Color( 10, 110, 255), sf::Color(255, 255, 255));
+
+
+    // The following objects were not discussed in this example code:
+    // - TextBox
+    // - Panel
+    // - SpriteSheet
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -177,20 +186,20 @@ int main()
             {
                 // Now check if the loading bar is full. If it is then close the window.
                 // You only had a pointer to the loading bar inside the init function.
-                // To get it back you must use getLoadingBar.
+                // To get it back you must use the get function.
                 // The "LOAD" name was chosen inside the init function.
-                if (callback.value == window.getLoadingBar("LOAD")->getMaximum())
+                if (callback.value == window.get<tgui::LoadingBar>("LOAD")->getMaximum())
                     window.close();
             }
         }
 
         // You only had a pointer to the object inside the init function.
-        // To get it back you must use one of the get functions.
+        // To get it back you must use the get function.
         // Here we first check if our checkbox is checked.
         // If it is checked then the value of the loading bar is incremented.
         // The "CHECK" and "LOAD" names were chosen inside the init function.
-        if (window.getCheckbox("CHECK")->isChecked())
-            window.getLoadingBar("LOAD")->incrementValue();
+        if (window.get<tgui::Checkbox>("CHECK")->isChecked())
+            window.get<tgui::LoadingBar>("LOAD")->incrementValue();
 
         window.clear();
 
