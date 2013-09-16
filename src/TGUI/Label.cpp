56c760509575cb986229f01995b92c955aac1968
Bruno Van de Velde <texus@tgui.eu>
2013-09-16 18:34:01 +0200

    Fixed small clipping bug in Label (fixes #16)

--- src/TGUI/Label.cpp
+++ src/TGUI/Label.cpp
@@ -372,7 +372,7 @@ namespace tgui
 
         // Get the global position
         sf::Vector2f topLeftPosition = states.transform.transformPoint(getPosition() - target.getView().getCenter() + (target.getView().getSize() / 2.f));
-        sf::Vector2f bottomRightPosition = states.transform.transformPoint(getPosition() + sf::Vector2f(m_Text.getLocalBounds().left, m_Text.getLocalBounds().top) + m_Size - target.getView().getCenter() + (target.getView().getSize() / 2.f));
+        sf::Vector2f bottomRightPosition = states.transform.transformPoint(getPosition() + m_Size - target.getView().getCenter() + (target.getView().getSize() / 2.f));
 
         // Get the old clipping area
         GLint scissor[4];
