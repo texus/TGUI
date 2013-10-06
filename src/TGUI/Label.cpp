b59c6a8b1d9e9700cab792f056237c1928164108
Bruno Van de Velde <texus@tgui.eu>
2013-10-06 16:08:59 +0200

    Make Label return the correct size again when there are multiple lines

--- src/TGUI/Label.cpp
+++ src/TGUI/Label.cpp
@@ -147,7 +147,7 @@ namespace tgui
         // Change the size of the label if necessary
         if (m_AutoSize)
         {
-            m_Size = sf::Vector2f(m_Text.findCharacterPos(m_Text.getString().getSize()).x - getPosition().x, m_Text.getLocalBounds().height);
+            m_Size = sf::Vector2f(m_Text.getLocalBounds().width, m_Text.getLocalBounds().height);
             m_Background.setSize(m_Size);
         }
     }
@@ -199,7 +199,7 @@ namespace tgui
         // Change the size of the label if necessary
         if (m_AutoSize)
         {
-            m_Size = sf::Vector2f(m_Text.findCharacterPos(m_Text.getString().getSize()).x - getPosition().x, m_Text.getLocalBounds().height);
+            m_Size = sf::Vector2f(m_Text.getLocalBounds().width, m_Text.getLocalBounds().height);
             m_Background.setSize(m_Size);
         }
     }
@@ -234,7 +234,7 @@ namespace tgui
         // Change the size of the label if necessary
         if (m_AutoSize)
         {
-            m_Size = sf::Vector2f(m_Text.findCharacterPos(m_Text.getString().getSize()).x - getPosition().x, m_Text.getLocalBounds().height);
+            m_Size = sf::Vector2f(m_Text.getLocalBounds().width, m_Text.getLocalBounds().height);
             m_Background.setSize(m_Size);
         }
     }
