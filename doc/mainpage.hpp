7713326e0308e778888e0a608f0bb33e0635e49c
Bruno Van de Velde <vdv_b@tgui.eu>
2014-03-28 14:19:59 +0100

    Update documentation page

--- doc/mainpage.hpp
+++ doc/mainpage.hpp
@@ -1,10 +1,11 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// \mainpage TGUI Documentation
 ///
-/// These pages contain the description of all usable functions and members in TGUI v0.6.\n
+/// These pages contain the description of all usable functions and members in TGUI v0.7-dev.\n
 /// The few functions that you should not use are hidden.
 ///
-/// Tutorials can be found on the tutorial section of my page (http://tgui.eu/tutorials/v06/).
+/// This is a development version for which no tutorials exist yet.
+/// You could have a look at the older tutorials (http://tgui.eu/tutorials/v06/), but not everything will be the same.
 ///
 /// More information about this project can be found on the TGUI home page (http://tgui.eu).
 ///
