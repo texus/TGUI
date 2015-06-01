fa50f4ee6e04f344af2e5e0c5f60e243a5ee31b8
Théo Verhelst <tverhelst@ulb.ac.be>
2015-06-01 15:32:09 +0200

    Layouts now use ClickableWidget instead of Button.

--- src/TGUI/HorizontalLayout.cpp
+++ src/TGUI/HorizontalLayout.cpp
@@ -5,39 +5,39 @@ namespace tgui
 
 HorizontalLayout::HorizontalLayout()
 {
-	m_callback.widgetType = "HorizontalLayout";
+    m_callback.widgetType = "HorizontalLayout";
 }
 
 HorizontalLayout::Ptr HorizontalLayout::create()
 {
-	return std::make_shared<HorizontalLayout>();
+    return std::make_shared<HorizontalLayout>();
 }
 
 HorizontalLayout::Ptr HorizontalLayout::copy(ConstPtr layout)
 {
-	if (layout)
-		return std::make_shared<HorizontalLayout>(*layout);
-	else
-		return nullptr;
+    if (layout)
+        return std::make_shared<HorizontalLayout>(*layout);
+    else
+        return nullptr;
 }
 
 void HorizontalLayout::updatePosition()
 {
-	const float sumRatio{std::accumulate(m_widgetsRatio.begin(), m_widgetsRatio.end(), 0.f)};
-	float actualPosition{0.f};
-	for (unsigned int i{0}; i < m_layoutWidgets.size(); ++i)
-	{
-		if (m_layoutWidgets[i])
-		{
-			m_layoutWidgets[i]->setSize(m_size.x*m_widgetsRatio[i]/sumRatio, m_size.y);
-			m_layoutWidgets[i]->setPosition(m_size.x*actualPosition, 0.f);
-			actualPosition += m_widgetsRatio[i]/sumRatio;
-		}
-	}
+    const float sumRatio{std::accumulate(m_widgetsRatio.begin(), m_widgetsRatio.end(), 0.f)};
+    float actualPosition{0.f};
+    for (unsigned int i{0}; i < m_layoutWidgets.size(); ++i)
+    {
+        if (m_layoutWidgets[i])
+        {
+            m_layoutWidgets[i]->setSize(m_size.x*m_widgetsRatio[i]/sumRatio, m_size.y);
+            m_layoutWidgets[i]->setPosition(m_size.x*actualPosition, 0.f);
+            actualPosition += m_widgetsRatio[i]/sumRatio;
+        }
+    }
 }
 
 tgui::Widget::Ptr HorizontalLayout::clone()
 {
-	return std::make_shared<HorizontalLayout>(*this);
+    return std::make_shared<HorizontalLayout>(*this);
 }
 }
--- src/TGUI/VerticalLayout.cpp
+++ src/TGUI/VerticalLayout.cpp
@@ -5,39 +5,39 @@ namespace tgui
 
 VerticalLayout::VerticalLayout()
 {
-	m_callback.widgetType = "VerticalLayout";
+    m_callback.widgetType = "VerticalLayout";
 }
 
 VerticalLayout::Ptr VerticalLayout::create()
 {
-	return std::make_shared<VerticalLayout>();
+    return std::make_shared<VerticalLayout>();
 }
 
 VerticalLayout::Ptr VerticalLayout::copy(ConstPtr layout)
 {
-	if (layout)
-		return std::make_shared<VerticalLayout>(*layout);
-	else
-		return nullptr;
+    if (layout)
+        return std::make_shared<VerticalLayout>(*layout);
+    else
+        return nullptr;
 }
 
 void VerticalLayout::updatePosition()
 {
-	const float sumRatio{std::accumulate(m_widgetsRatio.begin(), m_widgetsRatio.end(), 0.f)};
-	float actualPosition{0.f};
-	for (unsigned int i = 0; i < m_layoutWidgets.size(); ++i)
-	{
-		if (m_layoutWidgets[i])
-		{
-			m_layoutWidgets[i]->setSize(m_size.x, m_size.y*m_widgetsRatio[i]/sumRatio);
-			m_layoutWidgets[i]->setPosition(0.f, m_size.y*actualPosition);
-			actualPosition += m_widgetsRatio[i]/sumRatio;
-		}
-	}
+    const float sumRatio{std::accumulate(m_widgetsRatio.begin(), m_widgetsRatio.end(), 0.f)};
+    float actualPosition{0.f};
+    for (unsigned int i = 0; i < m_layoutWidgets.size(); ++i)
+    {
+        if (m_layoutWidgets[i])
+        {
+            m_layoutWidgets[i]->setSize(m_size.x, m_size.y*m_widgetsRatio[i]/sumRatio);
+            m_layoutWidgets[i]->setPosition(0.f, m_size.y*actualPosition);
+            actualPosition += m_widgetsRatio[i]/sumRatio;
+        }
+    }
 }
 
 tgui::Widget::Ptr VerticalLayout::clone()
 {
-	return std::make_shared<VerticalLayout>(*this);
+    return std::make_shared<VerticalLayout>(*this);
 }
 }
