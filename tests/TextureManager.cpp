70bab283d48d37afeb8f9ef38dac87f9df64370f
Bruno Van de Velde <vdv_b@tgui.eu>
2016-01-22 00:24:33 +0100

    Added tests for few untested lines TextureManager

---
+++ tests/TextureManager.cpp
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// TGUI - Texus's Graphical User Interface
+// Copyright (C) 2012-2015 Bruno Van de Velde (vdv_b@tgui.eu)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "catch.hpp"
+#include <TGUI/TextureManager.hpp>
+#include <TGUI/Texture.hpp>
+
+TEST_CASE("[TextureManager]") {
+    tgui::TextureManager textureManager;
+        
+    std::streambuf *oldbuf = sf::err().rdbuf(0);
+    tgui::Texture texture;
+    REQUIRE(!textureManager.getTexture(texture, "NonExistent.png"));
+    sf::err().rdbuf(oldbuf);
+
+    tgui::Texture texture1;
+    tgui::Texture texture2;
+    REQUIRE(textureManager.getTexture(texture1, "resources/image.png"));
+    REQUIRE(textureManager.getTexture(texture2, "resources/image.png"));
+    REQUIRE(texture1.getData() == texture2.getData());
+
+    REQUIRE_THROWS_AS(textureManager.copyTexture(std::make_shared<tgui::TextureData>()), tgui::Exception);
+    REQUIRE_NOTHROW(textureManager.copyTexture(texture1.getData()));
+
+    REQUIRE_THROWS_AS(textureManager.removeTexture(std::make_shared<tgui::TextureData>()), tgui::Exception);
+    REQUIRE_NOTHROW(textureManager.removeTexture(texture1.getData()));
+    REQUIRE_NOTHROW(textureManager.removeTexture(texture1.getData()));
+    REQUIRE_NOTHROW(textureManager.removeTexture(texture1.getData()));
+    REQUIRE_THROWS_AS(textureManager.removeTexture(texture1.getData()), tgui::Exception);
+}
