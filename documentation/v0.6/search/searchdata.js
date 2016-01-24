3a8ac602fe72da60b9d32a05e9b82d4b08551cd1
Bruno Van de Velde <vdv_b@tgui.eu>
2016-01-24 18:29:27 +0100

    Update 0.6 documentation to v0.6.10

---
+++ documentation/v0.6/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghiklmnoprstuvw~",
+  1: "abcegiklmprstw",
+  2: "t",
+  3: "abcdefghiklmoprstuw~",
+  4: "blrt",
+  5: "abcegklmprstw",
+  6: "abcfilmnrstuv",
+  7: "t"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "variables",
+  5: "enums",
+  6: "enumvalues",
+  7: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Variables",
+  5: "Enumerations",
+  6: "Enumerator",
+  7: "Pages"
+};
+
