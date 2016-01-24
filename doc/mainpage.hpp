5b10a8630670624d3e1686b04730f8875d93c7a6
Bruno Van de Velde <vdv_b@tgui.eu>
2016-01-24 16:54:50 +0100

    Corrected links in documentation main page

--- doc/mainpage.hpp
+++ doc/mainpage.hpp
@@ -4,8 +4,8 @@
 /// These pages contain the description of all usable functions and members in TGUI v0.6.\n
 /// The few functions that you should not use are hidden.
 ///
-/// Tutorials can be found on the tutorial section of my page (http://tgui.eu/tutorials/v06/).
+/// Tutorials can be found on the tutorial section of my page (https://tgui.eu/tutorials/v0.6/).
 ///
-/// More information about this project can be found on the TGUI home page (http://tgui.eu).
+/// More information about this project can be found on the TGUI home page (https://tgui.eu).
 ///
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
