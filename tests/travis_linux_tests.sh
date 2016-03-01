a3ff9616faf9ac7b52766ba8f36ad144ef6916c6
Bruno Van de Velde <vdv_b@tgui.eu>
2016-03-01 20:20:50 +0100

    Disable extra testing for WIP branch and only run partial unit tests

--- tests/travis_linux_tests.sh
+++ tests/travis_linux_tests.sh
@@ -1,25 +1,30 @@
 set -e
 
-export SFML_ROOT=$HOME/SFML-2.2_LINUX
+export SFML_ROOT=$HOME/SFML_LINUX
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 
-if [[ ! -d "$SFML_ROOT/lib" ]]; then
-  echo "$(tput setaf 3)Rebuilding SFML: no cache available$(tput sgr 0)"
+git clone --depth 1 https://github.com/SFML/SFML
 
-  wget -O SFML.tar.gz https://github.com/SFML/SFML/archive/2.2.tar.gz
-  tar -xzf SFML.tar.gz
-  cd SFML-2.2
+cd SFML
+if [[ ! -d "$SFML_ROOT/lib" || ! -f "$SFML_ROOT/revision_cache" || `git rev-parse HEAD` != `cat "$SFML_ROOT/revision_cache"` ]]; then
+  if [[ ! -d "$SFML_ROOT/lib" ]]; then
+    echo "$(tput setaf 3)Rebuilding SFML: no cache available$(tput sgr 0)"
+  else
+    echo "$(tput setaf 3)Rebuilding SFML: updating version$(tput sgr 0)"
+  fi
+
+  git rev-parse HEAD > "$SFML_ROOT/revision_cache"
   cmake -DCMAKE_INSTALL_PREFIX=$SFML_ROOT .
   make -j2
   make install
-  cd ..
 else
   echo "$(tput setaf 2)Using cached SFML directory$(tput sgr 0)"
 fi
+cd ..
 
-mkdir build_gcc-5
-cd build_gcc-5
+mkdir build
+cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -DTGUI_BUILD_TESTS=TRUE -DTGUI_USE_GCOV=TRUE ..
 make -j2
 cd tests/
