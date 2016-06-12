40ff3c92978563d7af8bccd7409dff2ae20686e0
Bruno Van de Velde <bruno@texus.me>
2016-06-12 14:45:05 +0200

    Added clipping to ListBox as it no longer contains a Panel to do it

--- src/TGUI/Widgets/ChatBox.cpp
+++ src/TGUI/Widgets/ChatBox.cpp
@@ -28,6 +28,8 @@
 #include <TGUI/Widgets/ChatBox.hpp>
 #include <TGUI/Loading/Theme.hpp>
 
+#include <SFML/OpenGL.hpp>
+
 #include <cassert>
 #include <cmath>
 
@@ -807,9 +809,42 @@ namespace tgui
         // Draw the background
         getRenderer()->draw(target, states);
 
+        const sf::View& view = target.getView();
+
+        // Calculate the scale factor of the view
+        float scaleViewX = target.getSize().x / view.getSize().x;
+        float scaleViewY = target.getSize().y / view.getSize().y;
+
+        // Get the global position
+        sf::Vector2f topLeftPosition = {((getAbsolutePosition().x - view.getCenter().x + (view.getSize().x / 2.f)) * view.getViewport().width) + (view.getSize().x * view.getViewport().left),
+                                        ((getAbsolutePosition().y - view.getCenter().y + (view.getSize().y / 2.f)) * view.getViewport().height) + (view.getSize().y * view.getViewport().top)};
+        sf::Vector2f bottomRightPosition = {(getAbsolutePosition().x + getSize().x - view.getCenter().x + (view.getSize().x / 2.f)) * view.getViewport().width + (view.getSize().x * view.getViewport().left),
+                                            (getAbsolutePosition().y + getSize().y - view.getCenter().y + (view.getSize().y / 2.f)) * view.getViewport().height + (view.getSize().y * view.getViewport().top)};
+
+        // Get the old clipping area
+        GLint scissor[4];
+        glGetIntegerv(GL_SCISSOR_BOX, scissor);
+
+        // Calculate the clipping area
+        GLint scissorLeft = std::max(static_cast<GLint>(topLeftPosition.x * scaleViewX), scissor[0]);
+        GLint scissorTop = std::max(static_cast<GLint>(topLeftPosition.y * scaleViewY), static_cast<GLint>(target.getSize().y) - scissor[1] - scissor[3]);
+        GLint scissorRight = std::min(static_cast<GLint>(bottomRightPosition.x * scaleViewX), scissor[0] + scissor[2]);
+        GLint scissorBottom = std::min(static_cast<GLint>(bottomRightPosition.y * scaleViewY), static_cast<GLint>(target.getSize().y) - scissor[1]);
+
+        if (scissorRight < scissorLeft)
+            scissorRight = scissorLeft;
+        else if (scissorBottom < scissorTop)
+            scissorTop = scissorBottom;
+
+        // Set the clipping area
+        glScissor(scissorLeft, target.getSize().y - scissorBottom, scissorRight - scissorLeft, scissorBottom - scissorTop);
+
         // Draw the text
         for (auto& line : m_lines)
             target.draw(line.text, states);
+
+        // Reset the old clipping area
+        glScissor(scissor[0], scissor[1], scissor[2], scissor[3]);
 
         // Draw the scrollbar if there is one
         if (m_scroll != nullptr)
