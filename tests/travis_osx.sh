e797a095eb0bf05acf1cf39c766e57674d112cad
Bruno Van de Velde <bruno@texus.me>
2016-09-15 16:00:56 +0200

    CI should not try to build i386 version of SFML as this will no longer work when using a newer SFML version

--- tests/travis_osx.sh
+++ tests/travis_osx.sh
@@ -9,7 +9,7 @@ if [[ ! -d "$SFML_ROOT/lib" ]]; then
   wget -O SFML.tar.gz https://github.com/SFML/SFML/archive/2.3.2.tar.gz
   tar -xzf SFML.tar.gz
   cd SFML-2.3.2
-  cmake -DCMAKE_INSTALL_PREFIX=$SFML_ROOT -DCMAKE_INSTALL_FRAMEWORK_PREFIX=$SFML_ROOT/lib -DCMAKE_OSX_ARCHITECTURES="i386;x86_64" -DSFML_BUILD_FRAMEWORKS=TRUE .
+  cmake -DCMAKE_INSTALL_PREFIX=$SFML_ROOT -DCMAKE_INSTALL_FRAMEWORK_PREFIX=$SFML_ROOT/lib -DSFML_BUILD_FRAMEWORKS=TRUE .
   make -j2
   make install
   cd ..
