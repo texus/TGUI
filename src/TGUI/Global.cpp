83930cff0d9c5cb3995817c242fb1cfca6b67cf7
Kurt Slagle <kurt_slagle@yahoo.com>
2017-09-27 12:56:57 -0500

    Change use of std::not1 and std::ptr_fun to lambdas due to c++17 deprecations (#77)

--- src/TGUI/Global.cpp
+++ src/TGUI/Global.cpp
@@ -166,8 +166,8 @@ namespace tgui
 
     std::string trim(std::string str)
     {
-        str.erase(str.begin(), std::find_if(str.begin(), str.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-        str.erase(std::find_if(str.rbegin(), str.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), str.end());
+        str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](int c) { return !std::isspace(c); }));
+        str.erase(std::find_if(str.rbegin(), str.rend(), [](int c) { return !std::isspace(c); }).base(), str.end());
         return str;
     }
 
