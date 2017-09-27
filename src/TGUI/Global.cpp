b865de7a91634b2c7ecfc9a7381d5eb434c668dc
Kurt Slagle <kurt_slagle@yahoo.com>
2017-09-27 12:01:53 -0500

    Add missing std:: before isspace

--- src/TGUI/Global.cpp
+++ src/TGUI/Global.cpp
@@ -165,8 +165,8 @@ namespace tgui
 
     std::string trim(std::string str)
     {
-        str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](int c) { return !isspace(c); }));
-        str.erase(std::find_if(str.rbegin(), str.rend(), [](int c) { return !isspace(c); }).base(), str.end());
+        str.erase(str.begin(), std::find_if(str.begin(), str.end(), [](int c) { return !std::isspace(c); }));
+        str.erase(std::find_if(str.rbegin(), str.rend(), [](int c) { return !std::isspace(c); }).base(), str.end());
         return str;
     }
 
