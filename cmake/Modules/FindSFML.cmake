1fc9cd57bc0526e2687918086c8fc1cc76d6a478
Bruno Van de Velde <bruno@texus.me>
2018-04-30 16:23:45 +0200

    Don't try compating the SFML version in FindSFML.cmake when Config.hpp was not found

--- cmake/Modules/FindSFML.cmake
+++ cmake/Modules/FindSFML.cmake
@@ -227,7 +227,7 @@ foreach(FIND_SFML_COMPONENT ${SFML_FIND_COMPONENTS})
 endforeach()
 
 # in case of static linking, we must also define the list of all the dependencies of SFML libraries
-if(SFML_STATIC_LIBRARIES)
+if(SFML_INCLUDE_DIR AND SFML_STATIC_LIBRARIES)
 
     # detect the OS
     if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
