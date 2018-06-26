9965b8187286bf853571dc3684523eee80ef0a08
Bruno Van de Velde <bruno@texus.me>
2018-06-26 19:42:30 +0200

    Updated documentation main page

--- doc/mainpage.hpp
+++ doc/mainpage.hpp
@@ -1,6 +1,11 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// \mainpage TGUI Documentation
 ///
-/// These pages contain the description of the classes and functions in TGUI 0.8-dev.\n
+/// These pages contain the description of all usable functions and members in TGUI 0.8.\n
 ///
+/// Make sure to also check the tutorials (https://tgui.eu/tutorials/0.8/) and example codes (https://tgui.eu/examples/0.8/).
+///
+/// More information about this project can be found on the TGUI home page (https://tgui.eu).
+///
+/// Do not hesitate to ask me questions on the forum (https://forum.tgui.eu) or by mail (texus@tgui.eu).
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
