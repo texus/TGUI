a87a4a3075d2a01111c986b19f6236102867ee05
Bruno Van de Velde <bruno@texus.me>
2018-07-28 16:51:56 +0200

    Fixed android test

--- tests/travis_android.sh
+++ tests/travis_android.sh
@@ -1,34 +1,49 @@
 set -e
 
-mkdir -p $ANDROID_NDK
-wget -nv https://dl.google.com/android/repository/android-ndk-r17b-linux-x86_64.zip
-unzip -o -q android-ndk-r17b-linux-x86_64.zip -d $HOME
+NDK_DIR=$HOME/android-ndk
+NDK_VER=r17b
 
+wget -nv https://dl.google.com/android/repository/android-ndk-${NDK_VER}-linux-x86_64.zip
+unzip -o -q android-ndk-${NDK_VER}-linux-x86_64.zip
+mv android-ndk-${NDK_VER} $NDK_DIR
+
+sudo apt purge cmake
+wget -nv https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.tar.gz
+tar -xvf cmake-3.12.0-Linux-x86_64.tar.gz > /dev/null
+sudo cp -r cmake-3.12.0-Linux-x86_64/bin /usr/
+sudo cp -r cmake-3.12.0-Linux-x86_64/share /usr/
+
+mkdir -p $HOME/sfml-cache
 git clone --depth 1 https://github.com/SFML/SFML
 cd SFML
 
-if [[ ! -d "$ANDROID_NDK/sources/sfml/lib" || ! -f "$ANDROID_NDK/sources/sfml/revision_cache" || `git rev-parse HEAD` != `cat "$ANDROID_NDK/sources/sfml/revision_cache"` ]]; then
-  if [[ ! -d "$ANDROID_NDK/sources/sfml/lib" ]]; then
+if [[ ! -d "$HOME/sfml-cache/lib" || ! -f "$HOME/sfml-cache/revision_cache" || `git rev-parse HEAD` != `cat "$HOME/sfml-cache/revision_cache"` ]]; then
+  if [[ ! -d "$HOME/sfml-cache/lib" ]]; then
     echo "$(tput setaf 3)Rebuilding SFML: no cache available$(tput sgr 0)"
   else
     echo "$(tput setaf 3)Rebuilding SFML: updating version$(tput sgr 0)"
   fi
 
-  git rev-parse HEAD > "$ANDROID_NDK/sources/sfml/revision_cache"
-  cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=$ANDROID_NDK -DCMAKE_ANDROID_ARCH_ABI=x86 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_BUILD_TYPE=Debug -DSFML_BUILD_AUDIO=FALSE  -DSFML_BUILD_NETWORK=FALSE .
+  git rev-parse HEAD > "$HOME/sfml-cache/revision_cache"
+  cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=$NDK_DIR -DCMAKE_ANDROID_ARCH_ABI=x86 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_BUILD_TYPE=Debug -DCMAKE_ANDROID_API=19 .
   make -j2
   make install
 else
   echo "$(tput setaf 2)Using cached SFML directory$(tput sgr 0)"
+  mkdir -p $NDK_DIR/sources/third_party/sfml
+  cp -R $HOME/sfml-cache/* $NDK_DIR/sources/third_party/sfml/
 fi
 cd ..
 
 mkdir build-android
 cd build-android
-cmake -DTGUI_OPTIMIZE_SINGLE_BUILD=TRUE -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=/home/texus/Documents/android/android-ndk-r13b/ -DCMAKE_ANDROID_ARCH_ABI=x86 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DTGUI_OPTIMIZE_SINGLE_BUILD=TRUE -DCMAKE_SYSTEM_NAME=Android -DCMAKE_ANDROID_NDK=$NDK_DIR -DCMAKE_ANDROID_ARCH_ABI=x86 -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_BUILD_TYPE=Debug ..
 make -j2
 make install
 cd ../examples/android
 android update project --name TGUI --path . --target android-23
-$ANDROID_NDK/ndk-build
+$NDK_DIR/ndk-build
 ant debug
+
+rm -R $HOME/sfml-cache/*
+mv $NDK_DIR/sources/third_party/sfml/* $HOME/sfml-cache/
