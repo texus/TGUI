d0e1a9c1340fc73131c38a78c159baaf837078e8
Bruno Van de Velde <vdv_b@tgui.eu>
2022-09-17 10:47:47 +0200

    Updated version in documentation

--- doc/mainpage.hpp
+++ doc/mainpage.hpp
@@ -1,9 +1,9 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// \mainpage TGUI Documentation
 ///
-/// These pages contain the description of all usable functions and members in TGUI 0.10.\n
+/// These pages contain the description of all usable functions and members in TGUI 1.0.\n
 ///
-/// Make sure to also check the tutorials (https://tgui.eu/tutorials/0.10/) and example codes (https://tgui.eu/examples/0.10/).
+/// Make sure to also check the tutorials (https://tgui.eu/tutorials/1.0/) and example codes (https://tgui.eu/examples/1.0/).
 ///
 /// More information about this project can be found on the TGUI home page (https://tgui.eu).
 ///
