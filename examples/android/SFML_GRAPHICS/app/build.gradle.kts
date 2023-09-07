3def87d7a545bc5b706a56b3497bc088a5a4dc0d
Bruno Van de Velde <vdv_b@tgui.eu>
2023-09-07 20:27:20 +0200

    Updated SFML Android example

---
+++ examples/android/SFML_GRAPHICS/app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+}
+
+val NDK_VERSION by extra(project.properties["NDK_VERSION"] as? String ?: "25.2.9519653")
+val ARCH_ABI by extra(project.properties["ARCH_ABI"] as? String ?: "arm64-v8a")
+
+android {
+    namespace = "eu.tgui.app"
+    ndkVersion = NDK_VERSION
+    compileSdk = 33
+    defaultConfig {
+        applicationId = "eu.tgui.app"
+        minSdk = 23
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        ndk {
+            abiFilters.add(ARCH_ABI)
+        }
+        externalNativeBuild {
+            cmake {
+                arguments.add("-DANDROID_STL=c++_shared")
+            }
+        }
+    }
+    packagingOptions {
+        jniLibs {
+            useLegacyPackaging = true  // Required for SFML 2.x with minSdk >= 23
+        }
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path("src/main/jni/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+}
--- examples/android/SFML_GRAPHICS/app/src/main/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-NDK_TOOLCHAIN_VERSION := clang
-APP_STL := c++_static
-APP_MODULES := sfml-activity-d tgui-activity-d app
