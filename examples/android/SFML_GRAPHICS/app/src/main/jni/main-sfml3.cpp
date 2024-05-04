c26611b599875b79eba4fc79ed7b1d2d7c148f1c
Bruno Van de Velde <vdv_b@tgui.eu>
2024-05-04 09:27:32 +0200

    Update android example code to SFML 3

---
+++ examples/android/SFML_GRAPHICS/app/src/main/jni/main-sfml3.cpp
@@ -0,0 +1,122 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// TGUI - Texus' Graphical User Interface
+// Copyright (C) 2012-2024 Bruno Van de Velde (vdv_b@tgui.eu)
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it freely,
+// subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented;
+//    you must not claim that you wrote the original software.
+//    If you use this software in a product, an acknowledgment
+//    in the product documentation would be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such,
+//    and must not be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
+
+// The background image will rotate with the screen
+void setBackground(tgui::Gui& gui, sf::Vector2u screenSize)
+{
+    gui.get("Landscape")->setSize(screenSize.x, screenSize.y);
+    gui.get("Portrait")->setSize(screenSize.x, screenSize.y);
+
+    if (screenSize.x > screenSize.y)
+    {
+        gui.get("Landscape")->setVisible(true);
+        gui.get("Portrait")->setVisible(false);
+    }
+    else
+    {
+        gui.get("Landscape")->setVisible(false);
+        gui.get("Portrait")->setVisible(true);
+    }
+}
+
+int main(int, char**)
+{
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "");
+    window.setFramerateLimit(30);
+
+    tgui::Gui gui(window);
+
+    auto picLandscape = tgui::Picture::create("Background-Landscape.png");
+    gui.add(picLandscape, "Landscape");
+
+    auto picPortrait = tgui::Picture::create("Background-Portrait.png");
+    gui.add(picPortrait, "Portrait");
+
+    // The button will quit the program
+    auto button = tgui::Button::create("Quit");
+    button->setPosition(50, 50);
+    button->setSize(200, 50);
+    button->onPress([&]{ window.close(); });
+    gui.add(button);
+
+    // Clicking on this edit box will open the keyboard and allow you to type in it
+    auto editBox = tgui::EditBox::create();
+    editBox->setPosition(50, 150);
+    editBox->setSize(400, 40);
+    editBox->setDefaultText("Enter text here...");
+    gui.add(editBox);
+
+    setBackground(gui, window.getSize());
+
+    // We shouldn't try drawing to the screen while in background
+    // so we'll have to track that. You can do minor background
+    // work, but keep battery life in mind.
+    bool active = true;
+
+    while (window.isOpen())
+    {
+        while (const auto event = active ? window.pollEvent() : window.waitEvent())
+        {
+            if (event.is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+            else if (const auto* keyEvent = event.getIf<sf::Event::KeyPressed>())
+            {
+                if (keyEvent->code == sf::Keyboard::Key::Escape)
+                    window.close();
+            }
+            else if (const auto* sizeEvent = event.getIf<sf::Event::Resized>())
+            {
+                setBackground(gui, {sizeEvent->size.x, sizeEvent->size.y});
+            }
+
+            // On Android MouseLeft/MouseEntered are (for now) triggered,
+            // whenever the app loses or gains focus.
+            else if (event.is<sf::Event::MouseLeft>())
+            {
+                active = false;
+            }
+            else if (event.is<sf::Event::MouseEntered>())
+            {
+                active = true;
+            }
+
+            gui.handleEvent(event);
+        }
+
+        if (active)
+        {
+            window.clear();
+            gui.draw();
+            window.display();
+        }
+        else // Application is in background
+            sf::sleep(sf::milliseconds(100));
+    }
+}
