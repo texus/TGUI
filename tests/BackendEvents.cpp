a29e7935c4ea64bc34624060f0568ea5ff41a373
Bruno Van de Velde <vdv_b@tgui.eu>
2025-03-07 22:06:12 +0100

    Skip gui.handleWindowEvents test until SFML/SFML#3454 is fixed

--- tests/BackendEvents.cpp
+++ tests/BackendEvents.cpp
@@ -80,6 +80,8 @@ TEST_CASE("[Backend events]")
         SECTION("SFML")
         {
 #if SFML_VERSION_MAJOR >= 3
+    // This test is temporarily disabled on Windows because of an issue in SFML (https://github.com/SFML/SFML/issues/3454)
+    #ifndef TGUI_SYSTEM_WINDOWS
             SECTION("handleWindowEvents")
             {
                 // We can't test whether the functions get called correctly (as that would require
@@ -92,6 +94,7 @@ TEST_CASE("[Backend events]")
                     [](auto&&) {}
                 );
             }
+    #endif
 #endif
 
             SECTION("KeyPressed")
