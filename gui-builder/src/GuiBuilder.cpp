dc8c516ad8e7d412fa7634e3b3b27df01240175a
Jesper Juhl <jesperjuhl76@gmail.com>
2026-01-14 20:53:25 +0100

    Remove some unused includes

--- gui-builder/src/GuiBuilder.cpp
+++ gui-builder/src/GuiBuilder.cpp
@@ -60,6 +60,7 @@
 #include "WidgetProperties/ToggleButtonProperties.hpp"
 #include "WidgetProperties/TreeViewProperties.hpp"
 #include "WidgetProperties/VerticalLayoutProperties.hpp"
+#include <thread>
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
