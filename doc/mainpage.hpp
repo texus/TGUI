92678b0e98903e6faa040079d7d23ccda71f8c62
Jesper Juhl <jesperjuhl76@gmail.com>
2026-02-19 17:15:16 +0100

    Fix up a URL in docs/mainpage.hpp
    The URL https://tgui.eu is strictly speaking not correct, it should be
    https://tgui.eu/

--- doc/mainpage.hpp
+++ doc/mainpage.hpp
@@ -5,7 +5,7 @@
 ///
 /// Make sure to also check the tutorials (https://tgui.eu/tutorials/1.0/) and example codes (https://tgui.eu/examples/1.0/).
 ///
-/// More information about this project can be found on the TGUI home page (https://tgui.eu).
+/// More information about this project can be found on the TGUI home page (https://tgui.eu/).
 ///
 /// Do not hesitate to ask me questions on the forum (https://forum.tgui.eu) or by mail (texus@tgui.eu).
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
