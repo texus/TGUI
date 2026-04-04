09bb3db5a12a9444bc64966bb4dd4d89b0c86bf4
Jesper Juhl <jesperjuhl76@gmail.com>
2026-04-04 16:24:25 +0200

    Apply clang-format to BackendRenderTarget.cpp

--- src/Backend/Renderer/BackendRenderTarget.cpp
+++ src/Backend/Renderer/BackendRenderTarget.cpp
@@ -94,14 +94,16 @@ namespace
         for (unsigned int i = 0; i < nrCornerPoints; ++i)
         {
             points.emplace_back(offset + radius + (radius * std::cos(twoPi * ((2 * (nrCornerPoints - 1)) + i) / nrPointsInCircle)),
-                                offset + size.y - radius - (radius * std::sin(twoPi * ((2 * (nrCornerPoints - 1)) + i) / nrPointsInCircle)));
+                                offset + size.y - radius
+                                    - (radius * std::sin(twoPi * ((2 * (nrCornerPoints - 1)) + i) / nrPointsInCircle)));
         }
 
         // Bottom right corner
         for (unsigned int i = 0; i < nrCornerPoints; ++i)
         {
             points.emplace_back(offset + size.x - radius + (radius * std::cos(twoPi * ((3 * (nrCornerPoints - 1)) + i) / nrPointsInCircle)),
-                                offset + size.y - radius - (radius * std::sin(twoPi * ((3 * (nrCornerPoints - 1)) + i) / nrPointsInCircle)));
+                                offset + size.y - radius
+                                    - (radius * std::sin(twoPi * ((3 * (nrCornerPoints - 1)) + i) / nrPointsInCircle)));
         }
 
         return points;
