3eb7097ce08cac0661c7dde0a92d17ebb1e85836
Jesper Juhl <jesperjuhl76@gmail.com>
2026-05-12 16:41:51 +0200

    Remove code that's only used when C++ standard is less than 17
    Commit 5bbf0aa6bcececee7b5649b30aa672985533c7d1 missed a spot.

--- src/Widgets/HorizontalWrap.cpp
+++ src/Widgets/HorizontalWrap.cpp
@@ -30,12 +30,6 @@
 
 namespace tgui
 {
-#if TGUI_COMPILED_WITH_CPP_VER < 17
-    constexpr char HorizontalWrap::StaticWidgetType[];
-#endif
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
     HorizontalWrap::HorizontalWrap(const char* typeName, bool initRenderer) :
         BoxLayout{typeName, false}
     {
